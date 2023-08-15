bf535fb7e4605851aa5a2e7af72168eb9c22bb56
Kyryl Andreiev <kyryl.andreiev_int@yubico.com>
2023-08-14 17:18:41 -0700

    remove god file

--- test.sh
+++ test.sh
@@ -1,6 +1,18 @@
-touch bad.good
-git add bad.good
-git commit -m "test1"
-rm bad.good
+touch good.file
+git add good.file
+git commit -m "add good file"
+rm good.file
 git add .
-git commit -m "test2"
+git commit -m "remove god file"
+touch bad.txt
+git add bad.txt
+git commit -m "add bad file"
+rm bad.txt
+git add .
+git commit -m "remove bad file"
+touch good.file
+git add good.file
+git commit -m "add good file"
+rm good.file
+git add .
+git commit -m "remove god file"
